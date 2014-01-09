06196793dd1c2b6932a1cae6fa7fbdfaa38b0eeb
Adam Ashley <aashley@adamashley.name>
2014-01-09 15:43:36 +0800

    Update dependancies

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'flapjack'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Adam Ashley'
+maintainer_email 'aashley@adamashley.name'
+license          'MIT'
 description      'Installs/Configures flapjack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'apt'
+depends          'logrotate'
