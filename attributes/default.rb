d97528a7a7271cc9e6dce2c955ba251e76b038aa
Adam Ashley <aashley@adamashley.name>
2014-01-09 15:43:14 +0800

    Basic install of flapjack package

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['flapjack']['repo']['uri'] = 'http://packages.flapjack.io/deb'
+default['flapjack']['repo']['components'] = 'main'
+default['flapjack']['repo']['signing-key'] = nil
+
+default['flapjack']['packages'] = %w{curl flapjack}
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,31 @@
 # Cookbook Name:: flapjack
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, Adam Ashley
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# FIXME: Hack taken from the flapjack puppet manifest, the flapjack packages
+# are not signed yet so we tell apt to install unauthenticated packages.
+# This is bad as soon as the packages are signed this will go away.
+file "/etc/apt/apt.conf.d/99auth" do
+    owner "root"
+    group "root"
+    mode "0644"
+    content "APT::Get::AllowUnauthenticated yes;"
+end
+
+apt_repository "flapjack" do
+    uri node['flapjack']['repo']['uri']
+    distribution node['lsb']['codename']
+    components [ "main" ]
+end
+
+node['flapjack']['packages'].each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+
+
